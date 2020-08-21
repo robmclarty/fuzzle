54d7c5feadb431c2bcee5f52ebfaf5804f248c36
Rob McLarty <hello@robmclarty.com>
2020-08-21 09:21:15 -0400

    fix: manifest file paths should use targetPath instead of always the default path

--- bin/fuzzle.js
+++ bin/fuzzle.js
@@ -12,58 +12,58 @@ const DEFAULT_MANIFEST_JS_NAME = 'rev-manifest-js.json'
 const DEFAULT_MANIFEST_CSS_NAME = 'rev-manifest-css.json'
 
 const revAssets = argv => {
-  const inputPath = argv[TARGET_ARG] || DEFAULT_BUILD_FOLDER
-  const manifestAssetsPath = `${ DEFAULT_BUILD_FOLDER }/${ DEFAULT_MANIFEST_ASSETS_NAME }`
+  const targetPath = argv[TARGET_ARG] || DEFAULT_BUILD_FOLDER
+  const manifestAssetsPath = `${targetPath}/${DEFAULT_MANIFEST_ASSETS_NAME}`
 
   return gulp.src([
-    `${ inputPath }/**/*`,
+    `${ targetPath }/**/*`,
     '!**/*.js',
     '!**/*.css',
     '!**/*.html'
   ])
     .pipe(rev())
-    .pipe(gulp.dest(inputPath))
+    .pipe(gulp.dest(targetPath))
     .pipe(rev.manifest(manifestAssetsPath))
     .pipe(gulp.dest('.'))
 }
 
 const revJS = argv => {
-  const inputPath = argv[TARGET_ARG] || DEFAULT_BUILD_FOLDER
-  const manifestAssetsPath = `${ DEFAULT_BUILD_FOLDER }/${ DEFAULT_MANIFEST_ASSETS_NAME }`
-  const manifestJSPath = `${ DEFAULT_BUILD_FOLDER }/${ DEFAULT_MANIFEST_JS_NAME }`
+  const targetPath = argv[TARGET_ARG] || DEFAULT_BUILD_FOLDER
+  const manifestAssetsPath = `${targetPath}/${DEFAULT_MANIFEST_ASSETS_NAME}`
+  const manifestJSPath = `${targetPath}/${DEFAULT_MANIFEST_JS_NAME}`
 
-  return gulp.src(`${ inputPath }/**/*.js`)
+  return gulp.src(`${targetPath}/**/*.js`)
     .pipe(revRewrite({ manifest: gulp.src(manifestAssetsPath) }))
     .pipe(rev())
-    .pipe(gulp.dest(inputPath))
+    .pipe(gulp.dest(targetPath))
     .pipe(rev.manifest(manifestJSPath))
     .pipe(gulp.dest('.'))
 }
 
 const revCSS = argv => {
-  const inputPath = argv[TARGET_ARG] || DEFAULT_BUILD_FOLDER
-  const manifestAssetsPath = `${ DEFAULT_BUILD_FOLDER }/${ DEFAULT_MANIFEST_ASSETS_NAME }`
-  const manifestCSSPath = `${ DEFAULT_BUILD_FOLDER }/${ DEFAULT_MANIFEST_CSS_NAME }`
+  const targetPath = argv[TARGET_ARG] || DEFAULT_BUILD_FOLDER
+  const manifestAssetsPath = `${targetPath}/${DEFAULT_MANIFEST_ASSETS_NAME}`
+  const manifestCSSPath = `${targetPath}/${DEFAULT_MANIFEST_CSS_NAME}`
 
-  return gulp.src(`${ inputPath }/**/*.css`)
+  return gulp.src(`${targetPath}/**/*.css`)
     .pipe(revRewrite({ manifest: gulp.src(manifestAssetsPath) }))
     .pipe(rev())
-    .pipe(gulp.dest(inputPath))
+    .pipe(gulp.dest(targetPath))
     .pipe(rev.manifest(manifestCSSPath))
     .pipe(gulp.dest('.'))
 }
 
 const revHTML = argv => {
-  const inputPath = argv[TARGET_ARG] || DEFAULT_BUILD_FOLDER
-  const manifestAssetsPath = `${ DEFAULT_BUILD_FOLDER }/${ DEFAULT_MANIFEST_ASSETS_NAME }`
-  const manifestJSPath = `${ DEFAULT_BUILD_FOLDER }/${ DEFAULT_MANIFEST_JS_NAME }`
-  const manifestCSSPath = `${ DEFAULT_BUILD_FOLDER }/${ DEFAULT_MANIFEST_CSS_NAME }`
+  const targetPath = argv[TARGET_ARG] || DEFAULT_BUILD_FOLDER
+  const manifestAssetsPath = `${targetPath}/${DEFAULT_MANIFEST_ASSETS_NAME}`
+  const manifestJSPath = `${targetPath}/${DEFAULT_MANIFEST_JS_NAME}`
+  const manifestCSSPath = `${targetPath}/${DEFAULT_MANIFEST_CSS_NAME}`
 
-  return gulp.src(`${ inputPath }/**/*.html`)
+  return gulp.src(`${targetPath}/**/*.html`)
     .pipe(revRewrite({ manifest: gulp.src(manifestAssetsPath) }))
     .pipe(revRewrite({ manifest: gulp.src(manifestCSSPath) }))
     .pipe(revRewrite({ manifest: gulp.src(manifestJSPath) }))
-    .pipe(gulp.dest(inputPath))
+    .pipe(gulp.dest(targetPath))
 }
 
 const revAll = argv => {
